ed35eeb6da2aff0bdfa3990cfa94f43dfeac54a1
leyjr <leyjr@mail.uc.edu>
2021-09-19 17:29:22 -0400

    Initial commit

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -4,6 +4,10 @@
 
 $IP = getIP
 $User  = $env:USERNAME
-$Body = "Test"
+$Date = Get-Date -Format "dddd MM/dd/yyyy"
+$Host = $env:COMPUTERNAME
+$Powershell = Get-Host | Select Object-Version
+
+$Body = "This machine's IP is $IP, User is $User, Hostname is $Host, the Powershell version is $Powershell, Today's data is $Date"
 
 Send-MailMessage -To "leyjr@mail.uc.edu" -From "leyjared@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $Body -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
