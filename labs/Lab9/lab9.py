6abe50c4f2a4a99141290378112bebea4fbf8a29
leyjr <leyjr@mail.uc.edu>
2021-12-02 10:54:03 -0500

    for lab9

---
+++ labs/Lab9/lab9.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+r = requests.get('http://localhost:3000')
+data = r.json()
+
+print(data[0]['name'],"is",data[0]['color'])
+print(data[1]['name'],"is",data[1]['color'])
+print(data[2]['name'],"is",data[2]['color'])
