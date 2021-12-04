7a9b0a17a4d828c5f7d49f2d192f33a7c100b4ac
leyjr <leyjr@mail.uc.edu>
2021-12-04 10:16:52 -0500

    removal

--- project3/SetBackground.ps1
+++
@@ -1,7 +0,0 @@
-﻿Function Set-DesktopBackground ($Value)
- {
-    Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
-    rundll32.exe user32.dll, UpdatePerUserSystemParameters
- }
-
-Set-DesktopBackground -value "C:\it3038c-scripts\project3\TestBackground.jpg"
